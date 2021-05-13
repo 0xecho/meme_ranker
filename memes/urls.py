0d8b6fedd6ef9d3d86041483613c11fe5609b378
0xecho <0xecho@proto-code.com>
2021-05-13 18:32:37 +0300

    Adds getting random memes

--- memes/urls.py
+++ memes/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from .views import RankMemesView
+from .views import RankMemesView, TopMemesView, get_two_memes, like_meme
 
 urlpatterns = [
-    path('rank/',RankMemesView.as_view(), name='rank')
+    path('rank/',RankMemesView.as_view(), name='rank'),
+    path('top/',TopMemesView.as_view(), name='top'),
+    path('get_two_memes/',get_two_memes, name='get_two_memes'),
+    path('like_meme/',like_meme, name='like_meme'),
 ]
--- memes/views.py
+++ memes/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+from django.http import JsonResponse
 
+from .models import Meme, Likes
+from random import choice
 # Create your views here.
 
 class RankMemesView(TemplateView):
 	template_name = "rank.html"
+
+class TopMemesView(DetailView):
+	model = Meme
+
+def get_two_memes(request):
+	meme_ids = Meme.objects.values_list('id', flat=True)
+
+	if len(meme_ids)<2:
+		return JsonResponse(data={"message":"Internal Error"}, status=500)
+
+	meme_1_id = choice(meme_ids)
+	meme_2_id = choice(meme_ids)
+	while meme_2_id == meme_1_id:
+		meme_2_id = choice(meme_ids)
+
+	meme_1 = Meme.objects.get(id=meme_1_id)
+	meme_2 = Meme.objects.get(id=meme_2_id)
+	respose_data = {
+		"meme_urls": [meme_1.image.url, meme_2.image.url]
+	}
+
+	return JsonResponse(data=respose_data)
+
+def like_meme(request):
+	return JsonResponse(data={"Nothing":"DEAR"})
+
